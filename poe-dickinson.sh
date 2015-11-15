5993f709104432fa9fb26e1a54b3b5bf29ed0a67
Richard Cui <rcui@umass.edu>
2015-11-15 13:17:51 -0500

    had the wrong filepaths oops

--- poe-dickinson.sh
+++ poe-dickinson.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar makeWords.jar src/main/resources/trainer1.txt src/main/resources/words.txt
+java -jar makeWords.jar src/main/resources/trainer2.txt src/main/resources/words.txt
 java -jar poetry-slam-1.0.0.jar src/main/resources/dickinson.txt src/main/resources/out.txt
